8aa94468350d341c319a7ebd2d662c211fe4e1ab
Zhen Deng <zhendeng.william@gmail.com>
2019-01-09 20:14:54 +1100

    upload file to git

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Stars extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Play Nine</h3>
+      </div>
+    );
+  }
+}
+
+class Game extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Play Nine`</h3>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Game />
+      </div>
+    );
+  }
+}
+
+export default App;
